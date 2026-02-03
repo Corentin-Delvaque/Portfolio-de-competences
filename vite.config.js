4c95917c612b44e434cdae5bfed65c705c4f565f
Corentin Delvaque <corentin.delvaque@icloud.com>
2026-02-03 17:10:14 +0100

    Suppression des options de construction dans la configuration Vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,5 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 export default defineConfig({
   base: "/Portfolio-de-competences",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        sae: resolve(__dirname, "sae-template.html"),
-        saeYears: resolve(__dirname, "sae-annees.html"),
-      },
-    },
-  },
 });
